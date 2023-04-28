8beece8c49246b4fe29cf2cb224f6e6e21f4bc50
muhammadfauzulhani16 <muhammadfauzulhanif2230511102@ummi.ac.id>
2023-04-28 14:51:49 +0700

    create album songs table migration

---
+++ migrations/1682665706749_album-songs-table.js
@@ -0,0 +1,36 @@
+exports.up = (pgm) => {
+  // membuat table collaborations
+  pgm.createTable('album_songs', {
+    id: {
+      type: 'CHAR(36)',
+      primaryKey: true
+    },
+    album_id: {
+      type: 'CHAR(36)',
+      notNull: true
+    },
+    song_id: {
+      type: 'CHAR(36)',
+      notNull: true
+    }
+  })
+
+  pgm.addConstraint(
+    'album_songs',
+    'unique_album_id_and_song_id',
+    'UNIQUE(album_id, song_id)'
+  )
+
+  pgm.addConstraint(
+    'album_songs',
+    'fk_album_songs.album_id_albums.id',
+    'FOREIGN KEY(album_id) REFERENCES albums(id) ON DELETE CASCADE'
+  )
+  pgm.addConstraint(
+    'album_songs',
+    'fk_album_songs.song_id_songs.id',
+    'FOREIGN KEY(song_id) REFERENCES songs(id) ON DELETE CASCADE'
+  )
+}
+
+exports.down = (pgm) => pgm.dropTable('album_songs')
