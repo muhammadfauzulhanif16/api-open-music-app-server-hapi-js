e5550e4cc7d1775bf562900fb43175a09f84e36b
muhammadfauzulhani16 <muhammadfauzulhanif2230511102@ummi.ac.id>
2023-04-29 11:35:45 +0700

    create authentication error

--- src/exceptions/index.js
+++ src/exceptions/index.js
@@ -1,5 +1,11 @@
 const ClientError = require('./ClientError')
 const NotFoundError = require('./NotFoundError')
 const InvariantError = require('./InvariantError')
+const AuthenticationError = require('./AuthenticationError')
 
-module.exports = { ClientError, InvariantError, NotFoundError }
+module.exports = {
+  ClientError,
+  InvariantError,
+  NotFoundError,
+  AuthenticationError
+}
